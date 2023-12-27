c39aa9693fa2b2f355bb6bdb3214048431c54f2a
beskilstark <ben.eskilstark@gmail.com>
2023-12-27 08:04:53 -0500

    add rest of photos, organize by project, add links to them, format for screen size and scroll position

--- js/TorrenceBanner.js
+++ js/TorrenceBanner.js
@@ -3,17 +3,42 @@ export default class TorrenceBanner extends HTMLElement {
   connectedCallback() {
     const fullSize = document.getElementById("fullsize");
     const smallSize = document.getElementById("smallsize");
+    const links = document.getElementById("links");
+    const space = document.getElementById("spacer");
+
+    let STATE = "BIG"; // || SMALL
 
     window.addEventListener("scroll", (ev) => {
-      if (window.scrollY > 300) {
-        fullSize.style.display = "none";
-        smallSize.style.display = "block";
-        this.style.boxShadow = "0 0 .5em rgba(0, 0, 0, .5)";
-      } else if (window.scrollY < 50) {
-        fullSize.style.display = "block";
-        smallSize.style.display = "none";
+      if (window.scrollY > 300 && STATE == "BIG") {
+        setSmall();
+      } else if (window.scrollY < 50  && STATE == "SMALL") {
+        setBig();
       }
     });
+
+    const setBig = () => {
+      STATE = "BIG";
+      fullSize.style.display = "block";
+      smallSize.style.display = "none";
+
+      this.style.justifyContent = "center";
+      spacer.style.display = "block";
+
+      links.style.flexDirection = "column";
+      links.style.alignItems = "end";
+    };
+
+    const setSmall = () => {
+      STATE = "SMALL";
+      fullSize.style.display = "none";
+      smallSize.style.display = "block";
+
+      this.style.justifyContent = "left";
+      spacer.style.display = "none";
+
+      links.style.flexDirection = "row";
+      links.style.alignItems = "center";
+    };
   }
 
 
